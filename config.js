11acab96986675df075c63af97a09c13daef8e2d
PutraManroee <67828924+PutraManroee@users.noreply.github.com>
2022-10-03 00:23:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6289526625512'],
-  ['6289526625512'],
-  ['6289526625512', 'Owner', true]
+  ['6282158393348'],
+  ['6282158393348'],
+  ['6282158393348', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@Fahzz'
-  var sticker_author = 'Fahzz'
+  var sticker_name = '@Manurung'
+  var sticker_author = 'Manurung'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Fahzz'
+global.wm = '© Manurung'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
